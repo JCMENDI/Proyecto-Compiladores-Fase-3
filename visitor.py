f00e747acd8a201d8f3bcc8430413ce68555c166
Upa211 <upa211@hotmail.com>
2025-05-27 19:23:13 -0600

    Creacion de clase para recorrer arbol  y escribir codigo
    soy upa

--- visitor.py
+++ visitor.py
@@ -10,3 +10,21 @@ class MiErrorListener(ErrorListener):
     def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
         print(f"Error de sintaxis en la línea {line}, columna {column}: {msg}")
         self.hubo_error = True
+
+
+class EvalVisitor(MiCompiladorVisitor):
+    def _init_(self):
+        self.memory = {}
+        self.functions = {}
+        self.output_code = []
+        self.current_function = None
+        self.indent_level = 0
+
+    def write_output(self, filename="output.py"):
+        with open(filename, "w", encoding="utf-8") as f:
+            f.write("# Código generado automáticamente\n")
+            f.write("\n".join(self.output_code))
+
+    def add_code_line(self, line):
+        indent = "    " * self.indent_level
+        self.output_code.append(f"{indent}{line}")
